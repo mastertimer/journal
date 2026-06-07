29ca8cc538bdf00fc86dd44eadf846939d988caa
Ivan <mastertimer@mail.ru>
2026-06-07 19:00:57 +0200

    timer

--- library/utils.h
+++ library/utils.h
@@ -1,5 +1,19 @@
 #pragma once
 
 #include "base.h"
+#include <chrono>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 std::wstring crypt(std::wstring s);
+
+struct timer
+{
+    using clock = std::chrono::steady_clock;
+    clock::time_point start = clock::now();
+
+    double  s() const { return std::chrono::duration<double>( clock::now() - start).count(); }
+    double ms() const { return std::chrono::duration<double, std::milli>(clock::now() - start).count(); }
+    double us() const { return std::chrono::duration<double, std::micro>(clock::now() - start).count(); }
+    double ns() const { return std::chrono::duration<double, std::nano>(clock::now() - start).count(); }
+};
